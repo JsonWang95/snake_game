d91115c49db9c4aab1981df8e3a6624101442808
Jason Wang <yuw545@ucsd.edu>
2021-10-11 17:58:56 -0700

    Cleanup Styles

--- scoreboard.py
+++ scoreboard.py
@@ -10,7 +10,7 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 270)
+        self.goto(0, 260)
         self.hideturtle()
         self.update_scoreboard()
 
@@ -22,3 +22,6 @@ class Scoreboard(Turtle):
         self.clear()
         self.update_scoreboard()
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
